219f46306ee3db994e84804d380b1a35d3a038c2
AlbertSolomon <solomonaalbert@gmail.com>
2023-01-07 13:10:55 +0200

    added app.py, utils dir with lang.py and config.txt...

---
+++ tubepy/app.py
@@ -0,0 +1,5 @@
+from pytube import YouTube
+from utils.lang import downloadstatus, url_input
+
+def download(youtube_url):
+    pass
--- utils/lang.py
+++ utils/lang.py
@@ -1,1 +1,8 @@
-downloadstatus = ["loading... 😒", " download successful 🥳"," download failed... 💔"]
+downloadstatus = {
+    "load": "loading... 😒",
+    "successful": " download successful 🥳",
+    "unsuccessful": "download failed... 💔", 
+}
+
+# this input variable will be used for testing purposes
+url_input = input("Enter Youtube Video URL here 👉🏾: ")
