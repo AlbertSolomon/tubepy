baf94a84d0303ac27dd99eb74fa42b75909885ed
AlbertSolomon <solomonaalbert@gmail.com>
2023-01-23 20:33:20 +0200

    further optmizing the code ⚡

--- tubepy/app.py
+++ tubepy/app.py
@@ -3,26 +3,27 @@ import ffmpeg
 from lang import read_config_file, progressive_vtags
 
 # try to read from config.json file where to store the downloaded video
-location:str = ''
 video_res = progressive_vtags.get("720p")
+
+location = read_config_file()
+preferred_location = location["download_location"]
+# print(preferred_location)
+
 def download(youtube_url):
-    
-    location = read_config_file()
-    preferred_location = location["download_location"]
-    # print(preferred_location)
-    
     youtube_file = YouTube(youtube_url)
     available_youtube_files = youtube_file.streams.filter(progressive=True)
     
+    # for testing purposes
     for yt_stream in available_youtube_files:
         print(yt_stream)
 
-    
     # downloading progressive videos ( allowing users to choose theie desird resolutions)yo
     progressive_res = youtube_file.streams.get_by_itag(video_res)
     progressive_res.download(preferred_location) 
     
 #? download audio files from youtube
+def audio_download(youtube_url):
+    pass 
 
 #? download Dynamic Adaptive Streaming over HTTP (DASH) and merge them with ffmpeg from youtube
 
