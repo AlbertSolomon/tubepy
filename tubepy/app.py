5578d9ce6aeefe0579a3fb83f6c912bc3c28f992
AlbertSolomon <solomonaalbert@gmail.com>
2023-01-23 20:57:32 +0200

    downloading audio files... 🔥

--- tubepy/app.py
+++ tubepy/app.py
@@ -4,6 +4,7 @@ from lang import read_config_file, progressive_vtags
 
 # try to read from config.json file where to store the downloaded video
 video_res = progressive_vtags.get("720p")
+audio_fq:str = ''
 
 location = read_config_file()
 preferred_location = location["download_location"]
@@ -11,9 +12,9 @@ preferred_location = location["download_location"]
 
 def download(youtube_url):
     youtube_file = YouTube(youtube_url)
-    available_youtube_files = youtube_file.streams.filter(progressive=True)
     
     # for testing purposes
+    available_youtube_files = youtube_file.streams.filter(progressive=True)
     for yt_stream in available_youtube_files:
         print(yt_stream)
 
@@ -23,7 +24,15 @@ def download(youtube_url):
     
 #? download audio files from youtube
 def audio_download(youtube_url):
-    pass 
+    youtube_file = YouTube(youtube_url)
+    
+    # for testing purposes
+    available_audiofiles = youtube_file.streams.filter(only_audio=True)
+    for available_audiofile in available_audiofiles:
+        print(available_audiofile)
+        
+    audio_file = youtube_file.streams.get_by_itag(audio_fq)
+    audio_file.download(preferred_location)
 
 #? download Dynamic Adaptive Streaming over HTTP (DASH) and merge them with ffmpeg from youtube
 
