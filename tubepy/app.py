642d38a241933e3a76069f7f7ed08c84c75bc0d5
AlbertSolomon <solomonaalbert@gmail.com>
2023-01-25 14:06:30 +0200

    implemented DASH function ⚠️ yet to be tested...

--- tubepy/app.py
+++ tubepy/app.py
@@ -1,6 +1,6 @@
 from pytube import YouTube
 import ffmpeg
-from lang import read_config_file, progressive_vtags
+from lang import read_config_file, progressive_vtags, clean_filename
 
 # try to read from config.json file where to store the downloaded video
 video_res = progressive_vtags.get("720p")
@@ -36,9 +36,22 @@ def audio_download(youtube_url):
 
 #? download Dynamic Adaptive Streaming over HTTP (DASH) and merge them with ffmpeg from youtube
 def DASH_download(youtube_url):
-    pass
+    youtube_file = YouTube(youtube_url)
     
+    try:        
+        youtube_file.streams.filter(res='1080p', progressive= False).first().download(preferred_location, filename="video.mp4")
+        youtube_file.streams.filter(abr='160kbps', progressive= False).first().download(preferred_location, filename="audio.mp3")
+        resolution = '1080p'
+    except:
+        youtube_file.streams.filter(res='720p', progressive= False).first().download(preferred_location, filename="video.mp4")
+        youtube_file.streams.filter(abr='128kbps', progressive= False).first().download(preferred_location, filename="audio.mp3")
+        resolution = '720p'
+        
+    audio = ffmpeg.input('audio.mp3')
+    video = ffmpeg.input('video.mp4')
     
+    filename = preferred_location + clean_filename(youtube_file.title) + 'mp4'
+    ffmpeg.output(audio, video, filename).run(overwrite_output=True)
         
 
 #? download video files from youtube with other formats
