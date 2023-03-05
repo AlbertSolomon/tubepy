5e90bcdec4a79b79572393f0d64cd2053a6614c0
AlbertSolomon <solomonaalbert@gmail.com>
2023-03-05 09:57:19 +0200

    file formating....

--- tubepy/settings.py
+++ tubepy/settings.py
@@ -1,8 +1,10 @@
 # all settings for the application
+import json
 import tkinter as tk
 from tkinter import filedialog
-import json
-from lang import read_config_file, download_location, empty
+
+from lang import download_location, empty, read_config_file
+
 
 # using new_location for testing purposes
 # new_location:str = "~/Music"
@@ -12,15 +14,17 @@ def change_download_location(new_location):
         default_location = read_config_file()
         default_location["download_location"] = new_location
 
-        with open('utilities/config.json', 'w') as file_location:
-            json.dump(default_location, file_location, indent=4 )
-        
+        with open("utilities/config.json", "w") as file_location:
+            json.dump(default_location, file_location, indent=4)
+
     except Exception:
         print(empty.get("empty_location"))
     else:
         default_location["download_location"] = download_location
-        
+
+
 # change_download_location(new_location)
+
 
 def download_path_settings():
     root = tk.Tk()
@@ -28,4 +32,3 @@ def download_path_settings():
 
     filepath = filedialog.askdirectory()
     change_download_location(filepath)
-
--- tubepy/test.py
+++ tubepy/test.py
@@ -1,11 +1,22 @@
-from app import download, audio_download, DASH_download, quick_download
-from settings import download_path_settings, change_download_location
 import asyncio
-from lang import url_input, validate_youtube_url, search_file_Availability, sample_url, file_verification, add_audio_stream_codes, file_existance
+
+from app import DASH_download, audio_download, download, quick_download
+from lang import (
+    add_audio_stream_codes,
+    file_existance,
+    file_verification,
+    sample_url,
+    search_file_Availability,
+    url_input,
+    validate_youtube_url,
+)
+from settings import change_download_location, download_path_settings
+
 
 # this input variable will be used for testing purposes
-def test_url_input() -> None:  
+def test_url_input() -> None:
     return input(url_input)
+
 
 # download_path_settings()
 # download(test_url_input())
@@ -19,7 +30,7 @@ def test_url_input() -> None:
 # print(file_existance(sample_url))
 # print(asyncio.run(search_file_Availability(sample_url)))
 # print(asyncio.run(file_verification(sample_url)))
-# 
+#
 audio_streams: list = add_audio_stream_codes(test_url_input())
 print(audio_streams[0])
 fstring = str(audio_streams[0])
@@ -27,13 +38,13 @@ print(fstring[9:19])
 
 # def stripping_audio_streams(audio_streams) -> list:
 #     new_audio_list: list= []
-#     
+#
 #     for audio_stream in audio_streams:
 #         temp_audio_file = str(audio_stream)
 #         new_audio_stream_file = temp_audio_file[9:19]
 #         new_audio_list.append(new_audio_stream_file)
-#         
+#
 #     return new_audio_list
-# 
-# nwLists = stripping_audio_streams(audio_streams)    
+#
+# nwLists = stripping_audio_streams(audio_streams)
 # print(nwLists)
