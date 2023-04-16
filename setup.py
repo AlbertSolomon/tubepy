b353f272fb47252e0283073af8b0aae9230f265d
AlbertSolomon <solomonaalbert@gmail.com>
2023-04-16 23:53:18 +0200

    OMGgit add .! it works 😅, 🚀🚀🚀 building exe for windows works..

--- setup.py
+++ setup.py
@@ -1,18 +1,34 @@
 from cx_Freeze import setup, Executable
-
-# Dependencies are automatically detected, but it might need
-# fine tuning.
-build_options = {'packages': [], 'excludes': []}
-
 import sys
-base = 'Win32GUI' if sys.platform=='win32' else None
 
+packages = ['tkinter','customtkinter', 'tubepy', 'pytube']
+
+# Add 'tubepy' to the path to allow Python to import modules from it at runtime
+sys.path.append('tubepy')
+
+# Define any extra files or directories to include in the build
+#includefiles = ['utils/config.json']
+includefiles = ["tubepy/", "utilities/", "tubepy/__init__.py", "tubepy/lang.py", "tubepy/app.py","tubepy/settings.py","tubepy/", "assets/"]
+
+
+# Define the build options
+build_options = {
+    'packages': packages,
+    'excludes': ['requirements.txt', '.github'],
+    'include_files': includefiles,
+}
+
+# Define the executablesy
+base = 'Win32GUI' if sys.platform == 'win32' else None
 executables = [
-    Executable('tubepy/setup.py', base=base, target_name = 'tubepy.msi')
+    Executable('tubepy/setup.py', base=base, target_name='tubepy.msi')
 ]
 
-setup(name='tubepy',
-      version = '1.0.0-beta',
-      description = 'YouTube video downloader in python 😎',
-      options = {'build_exe': build_options},
-      executables = executables)
+# Define the setup parameters
+setup(
+    name='tubepy',
+    version='1.0.0-beta',
+    description='YouTube video downloader in python 😎',
+    options={'build_exe': build_options},
+    executables=executables
+)
