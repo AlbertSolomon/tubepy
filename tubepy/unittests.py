8e01c9b3c854c5abf6b9efadf207b126c673d8fa
AlbertSolomon <solomonaalbert@gmail.com>
2023-08-10 11:27:52 +0200

    added test cases 🐧

--- tubepy/unittests.py
+++ tubepy/unittests.py
@@ -3,3 +3,5 @@ from .lang import validate_youtube_url
 
 def test_url_validation():
     assert(validate_youtube_url("https://www.youtube.com/watch?v=r6tH55syq0o")) == True
+    assert(validate_youtube_url("https://www.youtube.com/shorts/mBqK_-L-PVg")) == True
+    assert(validate_youtube_url("http://linuxcommand.org/lc3_writing_shell_scripts.php")) == False
